802121e4c567bf33e3b2f3d84f853552dd248363
Natal Lima <nataldelima@gmail.com>
2026-07-23 09:51:49 -0400

    Tornando o site responsivo

--- js/script.js
+++ js/script.js
@@ -6,6 +6,40 @@ window.addEventListener('scroll', () => {
 
 
 
+// mobile menu
+const hamburgerBtn = document.getElementById('hamburgerBtn');
+const mobileMenu = document.getElementById('mobileMenu');
+hamburgerBtn.addEventListener('click', (e) => {
+    e.stopPropagation();
+
+    mobileMenu.classList.toggle("open");
+    hamburgerBtn.classList.toggle("active");
+});
+mobileMenu.querySelectorAll('a').forEach(a => {
+    a.addEventListener('click', () => {
+        mobileMenu.classList.remove('open')
+        mobileMenu.classList.remove('active')
+    });
+});
+
+
+document.addEventListener("click", (e) => {
+    if (
+        mobileMenu.classList.contains("open") &&
+        !mobileMenu.contains(e.target) &&
+        !hamburgerBtn.contains(e.target)
+    ) {
+        mobileMenu.classList.remove("open");
+        hamburgerBtn.classList.remove("active");
+    }
+});
+
+window.addEventListener("resize", () => {
+    if (window.innerWidth > 980) {
+        mobileMenu.classList.remove("open");
+        hamburgerBtn.classList.remove("active");
+    }
+});
 
 
 // scroll reveal
